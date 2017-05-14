9551cb95c78c9bccb345102a0700906973bbad07
xjose97x <joseignacioescudero97@hotmail.com>
2017-05-14 10:42:49 -0500

    Created a functional view.

--- client/app/scripts/group/controller.js
+++ client/app/scripts/group/controller.js
@@ -1,88 +1,114 @@
 'use strict';
 
 angular.module('Group')
-.controller('group', function ($scope) {
+    .controller('group', function($scope) {
 
-  $scope.controller_loaded = 'Group loaded!';
-  $scope.multiply = function(nums){
-    var result = 1;
-    for(var i = 0; i < nums.length; i++){
-      result *= nums[i];
-    }
-    return result;
-  };
+        $scope.controller_loaded = 'Group loaded!';
+        /**
+         * Returns the result of the multiplication of the array.
+         * @param {Array<Number>} nums
+         * @return {Number} The result.
+         */
+        $scope.multiply = function(nums) {
+            var result = 1;
+            nums.forEach(function(num) {
+                result *= num;
+            });
+            return result;
+        };
 
-  /**
-   * Returns whether there's an error or not.
-   * 
-   * @param {Array<Number>} value Current Value
-   * @return {Number} The error number.
-   */
-  function checkErrors(value){
-    if(!(value[0] >= 1000 && value[0] <= 1999)){
-      return -1;
-    }
-    else if(!(value[1] >= 2000 && value[1] <= 2999)){
-      return -2;
-    }
-    return 0;
-  }
-  $scope.count = function(arg){
-    var repeatedValues = {},
-        error;
-    var result = arg.map(function(value, index){
-      error = checkErrors(value);
-      if(error< 0){
-        return error;
-      }
-      value.map(function(currVal, i){
-        //Handling
-        if(repeatedValues[currVal] >= 0){
-          repeatedValues[currVal]++;
+        /**
+         * Returns whether there's an error or not.
+         * 
+         * @param {Array<Number>} value Current Value
+         * @return {Number} The error number.
+         */
+        function checkErrors(value) {
+            if (!(value[0] >= 1000 && value[0] <= 1999)) {
+                return -1;
+            } else if (!(value[1] >= 2000 && value[1] <= 2999)) {
+                return -2;
+            }
+            return 0;
         }
-        else{
-          repeatedValues[currVal] = 1;
-        }
-      });
-      return repeatedValues;
+        /**
+         * Counts how many elements are
+         * @param {Array<Array<Number>>} arg
+         * @return {Object} result
+         */
+        $scope.count = function(arg) {
+            var repeatedValues = {},
+                error;
+            var result = arg.map(function(value) {
+                error = checkErrors(value);
+                if (error < 0) {
+                    return error;
+                }
+                value.map(function(currVal) {
+                    //Handling
+                    if (repeatedValues[currVal] >= 0) {
+                        repeatedValues[currVal]++;
+                    } else {
+                        repeatedValues[currVal] = 1;
+                    }
+                });
+                return repeatedValues;
+            });
+            return result[0];
+        };
+        /**
+         * Handles kata main functionality
+         */
+        $scope.kata = function(arg) {
+            //Count
+            var repeatedValues = $scope.count(arg);
+            //Error
+            if (repeatedValues < 0) {
+                return repeatedValues;
+            }
+            var result = [];
+            //Select 1 per team;
+            arg.map(function(value) {
+                var first = value[0],
+                    second = value[1],
+                    firstItem = repeatedValues[first],
+                    secondItem = repeatedValues[second];
+                if (firstItem > secondItem) {
+                    if (result.indexOf(first) === -1) {
+                        result.push(first);
+                    }
+                } else if (result.indexOf(second) === -1) {
+                    result.push(second);
+                }
+            });
+            return result;
+        };
+
+        $scope.employee = {
+            //For ng-model to work it needs to be an array of objects          
+            groups: [
+                { 'first': '', 'second': '' }
+            ],
+            add: function() {
+                this.groups.push({ 'first': '', 'second': '' });
+            },
+            remove: function(index) {
+                this.groups.splice(index, 1);
+            },
+            process: function() {
+                var result = [];
+                this.groups.map(function(elem) {
+                    result.push([elem.first, elem.second]);
+                });
+                $scope.result = $scope.kata(result);
+            }
+        };
+
+    })
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/group', {
+                templateUrl: 'scripts/group/views/group.html',
+                controller: 'group'
+            });
     });
-    return result[0];
-  }
-
-  $scope.kata = function(arg){
-    //Count
-    var repeatedValues = $scope.count(arg);
-    //Error
-    if(repeatedValues < 0){
-      return repeatedValues;
-    }
-
-    var result = [];
-    
-    //Select 1 per team;
-    for(var i = 0; i < arg.length; i++){
-      var first = arg[i][0],
-          second = arg[i][1];
-      var firstItem =repeatedValues[first],
-          secondItem = repeatedValues[second]; 
-      
-      if(firstItem > secondItem){
-        if(result.indexOf(first) == -1){          
-          result.push(first);          
-        }
-      }
-      else if(result.indexOf(second) == -1){
-        result.push(second);
-      }
-    }
-    return result;
-  };
-  
-})
-.config(function ($routeProvider) {
-  $routeProvider
-  .when('/group', {
-    templateUrl: 'scripts/group/views/group.html',
-    controller: 'group'
-  });
-});
--- client/app/scripts/group/specs/controller_spec.js
+++ client/app/scripts/group/specs/controller_spec.js
@@ -1,119 +1,121 @@
 'use strict';
 
-describe('Controller: select group', function () {
+describe('Controller: select group', function() {
 
-  beforeEach(module('Group'));
+    beforeEach(module('Group'));
 
-  var controller;
-  var scope;
+    var controller;
+    var scope;
 
-  beforeEach(inject(function ($rootScope, $controller) {
-    scope = $rootScope.$new();
-    controller = $controller('group', { $scope: scope });
-  }));
-
-  describe('On instance', function () {
-    it('should set "controller_loaded" variable in scope', function () {
-      expect(scope.controller_loaded).toContain('loaded');
-    });
-
-    it('should be able to multiply" variable in scope', function () {
-      var response  = scope.multiply([4,5,6]);
-      expect(response).toBe(4*5*6);
-
-      var response  = scope.multiply([0]);
-      expect(response).toBe(0);
-    });
-    
-    iit('kata: count', function(){
-      var arg = [[1000, 2000]];
-      var result = scope.count(arg);
-      expect(result).toEqual({1000: 1, 2000: 1});
-      arg = [
-        [100, 2000],
-      ];
-      result = scope.count(arg);
-      expect(result).toBe(-1);
-      arg = [
-        [1000, 200],
-      ];
-      result = scope.count(arg);
-      expect(result).toBe(-2);
-    });
-
-    iit('kata: handle errors', function(){
-      var result;
-      //First employee num incorrect
-      var err1 = [
-        [4000, 2011]
-      ];
-      result = scope.kata(err1);
-      expect(result).toBe(-1);
-      
-      //Second employee num incorrect
-      var err2 = [
-        [1004, 300]
-      ];
-      result = scope.kata(err2);
-      expect(result).toBe(-2);
-    });
-    iit('kata: real cases', function(){
-      var result;
-      var groups = [
-        [1009, 2011],
-        [1017, 2011]
-      ];
-      result = scope.kata(groups);
-      expect(result).toEqual([2011]);
-
-      var groups2 = [
-        [1009, 2000],
-        [1009, 2001],
-        [1002, 2002],
-        [1003, 2002]
-      ];
-      result = scope.kata(groups2);
-      expect(result).toEqual([1009, 2002]);
-
-      var groups3 = [
-        [1002, 2008],
-        [1006, 2008],
-        [1002, 2008],
-        [1002, 2080]
-      ];
-      result = scope.kata(groups3);
-      expect(result).toEqual([2008 ,1002]);
-    });
-  });
-
-  describe('when going to /group', function () {
-
-    var route, location, rootScope, httpBackend;
-
-    beforeEach(inject(function ($route, $location, $rootScope, $httpBackend) {
-      route = $route;
-      location = $location;
-      rootScope = $rootScope;
-      httpBackend = $httpBackend;
-
-      httpBackend.when('GET', 'scripts/group/views/group.html').respond('<div></div>');
+    beforeEach(inject(function($rootScope, $controller) {
+        scope = $rootScope.$new();
+        controller = $controller('group', { $scope: scope });
     }));
 
-    afterEach(function () {
-      httpBackend.verifyNoOutstandingExpectation();
-      httpBackend.verifyNoOutstandingRequest();
+    describe('On instance', function() {
+        it('should set "controller_loaded" variable in scope', function() {
+            expect(scope.controller_loaded).toContain('loaded');
+        });
     });
+    describe('Kata cases', function() {
+        it('should be able to multiply" variable in scope', function() {
+            var response = scope.multiply([4, 5, 6]);
+            expect(response).toBe(4 * 5 * 6);
 
-    it('should use minesweeper.html and controller', function () {
-      expect(route.current).toBeUndefined();
+            response = scope.multiply([0]);
+            expect(response).toBe(0);
+        });
 
-      location.path('/group');
+        it('kata: count', function() {
+            var arg = [
+                [1000, 2000]
+            ];
+            var result = scope.count(arg);
+            expect(result).toEqual({ 1000: 1, 2000: 1 });
+            arg = [
+                [100, 2000],
+            ];
+            result = scope.count(arg);
+            expect(result).toBe(-1);
+            arg = [
+                [1000, 200],
+            ];
+            result = scope.count(arg);
+            expect(result).toBe(-2);
+        });
 
-      httpBackend.flush();
+        it('kata: handle errors', function() {
+            var result;
+            //First employee num incorrect
+            var err1 = [
+                [4000, 2011]
+            ];
+            result = scope.kata(err1);
+            expect(result).toBe(-1);
 
-      expect(route.current.templateUrl).toBe('scripts/group/views/group.html');
-      expect(route.current.controller).toBe('group');
+            //Second employee num incorrect
+            var err2 = [
+                [1004, 300]
+            ];
+            result = scope.kata(err2);
+            expect(result).toBe(-2);
+        });
+        it('kata: real cases', function() {
+            var result;
+            var groups = [
+                [1009, 2011],
+                [1017, 2011]
+            ];
+            result = scope.kata(groups);
+            expect(result).toEqual([2011]);
+
+            var groups2 = [
+                [1009, 2000],
+                [1009, 2001],
+                [1002, 2002],
+                [1003, 2002]
+            ];
+            result = scope.kata(groups2);
+            expect(result).toEqual([1009, 2002]);
+
+            var groups3 = [
+                [1002, 2008],
+                [1006, 2008],
+                [1002, 2008],
+                [1002, 2080]
+            ];
+            result = scope.kata(groups3);
+            expect(result).toEqual([2008, 1002]);
+        });
     });
-  });
+    describe('when going to /group', function() {
+
+        var route, location, rootScope, httpBackend;
+
+        beforeEach(inject(function($route, $location, $rootScope, $httpBackend) {
+            route = $route;
+            location = $location;
+            rootScope = $rootScope;
+            httpBackend = $httpBackend;
+
+            httpBackend.when('GET', 'scripts/group/views/group.html').respond('<div></div>');
+        }));
+
+        afterEach(function() {
+            httpBackend.verifyNoOutstandingExpectation();
+            httpBackend.verifyNoOutstandingRequest();
+        });
+
+        it('should use minesweeper.html and controller', function() {
+            expect(route.current).toBeUndefined();
+
+            location.path('/group');
+
+            httpBackend.flush();
+
+            expect(route.current.templateUrl).toBe('scripts/group/views/group.html');
+            expect(route.current.controller).toBe('group');
+        });
+    });
 
 });
